d4887da7353bad4d15e73c5bc8090d38eda0d30d
Surabi <surabi.balakrishnan@csquare.in>
2025-06-12 14:49:38 +0530

    Available cars menu and pages added

---
+++ CarRental/BPO/AvailableCarsBPO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarRental.Helpers;
+using Newtonsoft.Json;
+
+namespace CarRental.BPO
+{
+    public class AvailableCarsBPO 
+    {
+        public static string Conn = CommonConnection.Conn;
+        public static string GetAvailableCars()
+        {
+            try
+            {
+                string query = @"SELECT c_car_id,c_model_name, c_type, n_year, n_rate_per_day, 'Available' as status,c_licence_plate, c_image_path FROM dba.car_mst;";
+                DataTable dt = SqlHelper.ExecuteDataset(Conn, CommandType.Text, query).Tables[0];
+                return JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex)
+            {
+                return JsonConvert.SerializeObject(new { error = ex.Message, stack = ex.StackTrace });
+            }
+        }
+    }
+}
